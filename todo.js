719cfb6cf25466b5eb7b3d7ad9ee94f17d570f1c
loffis <loffe.knutsson@gmail.com>
2020-09-09 22:36:01 +0200

    Add Todo object.

--- todo.js
+++ todo.js
@@ -1,10 +1,29 @@
 let form = document.getElementById("input-id");
 let divTasks = document.getElementById("tasks");
 
+var id = 0;
+var task = "";
+var isDone = false;
+
+function Todo(task) {
+    this.id = ++id;
+    this.task = task;
+    this.isDone = false;
+}
+
 form.addEventListener("submit", onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
     console.log("Your input: " + event.target.elements.task.value);
+    task = event.target.elements.task.value;
+    console.log("task: " + task);
+    createTodo(task);
+    event.target.reset();
+}
+
+function createTodo(task) {
+    let newTodo = new Todo(task);
+    console.log(newTodo);
 
 }
