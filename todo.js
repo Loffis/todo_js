7b56b0d3b060d04b479cc67288166d224dcb7c35
loffis <loffe.knutsson@gmail.com>
2020-09-10 02:09:13 +0200

    Add create todos and view all todos

--- todo.js
+++ todo.js
@@ -1,9 +1,12 @@
+let todos = [];
 let form = document.getElementById("input-id");
 let divTasks = document.getElementById("tasks");
+let displayedTodos = document.createElement("p");
+let viewButton = document.getElementById("btn-view");
 
-var id = 0;
-var task = "";
-var isDone = false;
+let id = 0;
+let task = "";
+let isDone = false;
 
 function Todo(task) {
     this.id = ++id;
@@ -12,18 +15,28 @@ function Todo(task) {
 }
 
 form.addEventListener("submit", onSubmit);
+viewButton.addEventListener("click", displayTodos);
 
 function onSubmit(event) {
     event.preventDefault();
-    console.log("Your input: " + event.target.elements.task.value);
     task = event.target.elements.task.value;
-    console.log("task: " + task);
     createTodo(task);
     event.target.reset();
 }
 
 function createTodo(task) {
     let newTodo = new Todo(task);
-    console.log(newTodo);
+    todos.push(newTodo);
+}
+
+function displayTodos() {
+    displayedTodos.innerHTML = "";
+    for (let i = 0; i < todos.length; i++) {
+        displayedTodos.innerHTML += "<br>ID: " + todos[i].id +
+            "<br>Finished: " + todos[i].isDone +
+            "<br>Task: " + todos[i].task + "<br>";
+    }
+
+    divTasks.appendChild(displayedTodos);
 
 }
